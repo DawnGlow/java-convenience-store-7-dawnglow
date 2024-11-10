9fadfa38bfaf36d5efc658019ad677c78a74280e
DawnGlow <ferrater1013@gmail.com>
2024-11-10 21:20:36 +0900

    feat: 프로모션 상품 엔티티 구현
    프로모션 상품 엔티티를 구현합니다.
    
    1. 프로모션 재고가 충분한지
    2. 일반 결제를 몇 개 해야 하는지
    3. PromotionInfo 기능 위임

---
+++ src/main/java/store/domain/builder/PromotionItemBuilder.java
@@ -0,0 +1,37 @@
+package store.domain.builder;
+
+import store.domain.PromotionInfo;
+import store.domain.PromotionItem;
+
+public class PromotionItemBuilder {
+    private String name;
+    private int price;
+    private int stock;
+
+    private PromotionInfo promotionInfo;
+
+    public PromotionItemBuilder name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PromotionItemBuilder price(int price) {
+        this.price = price;
+        return this;
+    }
+
+    public PromotionItemBuilder stock(int stock) {
+        this.stock = stock;
+        return this;
+    }
+
+    public PromotionItemBuilder promotionInfo(PromotionInfo promotionInfo) {
+        this.promotionInfo = promotionInfo;
+        return this;
+    }
+
+    public PromotionItem build() {
+        return new PromotionItem(name, price, stock, promotionInfo);
+    }
+
+}
