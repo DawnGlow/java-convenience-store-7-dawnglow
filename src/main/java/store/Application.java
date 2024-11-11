6616aabd2e763e95f9433b8c7eea92cf7837fb3d
DawnGlow <ferrater1013@gmail.com>
2024-11-11 23:22:58 +0900

    refactor: Application 최종 수정
    리팩터링 및 구현한 기능으로 통합합니다.

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,12 @@
 package store;
 
+import store.controller.StoreController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig appConfig = new AppConfig();
+        appConfig.initController().initData();
+        StoreController storeController = appConfig.storeController();
+        storeController.orderProcess();
     }
 }
